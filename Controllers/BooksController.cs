707d82280e39ffa66fca4e892151bcc6c1d06dd6
VirtiShah <shahvirti6794@gmail.com>
2017-05-01 23:45:38 +0530

    Add files via upload

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIServiceBooks.Models;
+
+namespace WebAPIServiceBooks.Controllers
+{
+    public class BooksController : ApiController
+    {
+        Books[] books = new Books[]
+         {
+             new Books { BookID = 1, BookName = "BN1", 
+                           BookAuthorName= "Author1", Price = 400 },
+      new Books { BookID = 1, BookName = "BN2", 
+                           BookAuthorName= "Author2", Price = 450 },
+new Books { BookID = 1, BookName = "BN3", 
+                           BookAuthorName= "Author3", Price = 300 },
+new Books { BookID = 1, BookName = "BN4", 
+                           BookAuthorName= "Author4", Price = 520 }, 
+    };
+        public IEnumerable<Books> GetBooks()
+        {
+            return books;
+        }
+
+    }
+}
---
+++ Models/Books.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIServiceBooks.Models
+{
+    public class Books
+    {
+        public int BookID { get; set; }
+        public string BookName { get; set; }
+        public string BookAuthorName { get; set; }
+        public int Price { get; set; }
+ 
+    }
+}
